bd366c88b37a52cafd50228f0bc75ce6173e1163
johnayuk <john1234ayuk@gmail.cm>
2021-03-25 21:37:02 +0100

    add DB_USERNAME on the .env and Auth::check() && in the admin middleware

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
-
+use App\User;
 class AdminMiddleware
 {
     /**
@@ -16,7 +16,7 @@ class AdminMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if(Auth::user()->role == 'admin'){
+        if(Auth::check() && Auth::user()->role == 'admin'){
 
             return $next($request);
 
